5454f85723691e3c51db5dfe3240eac6c318356e
omarkhaled <okoros192000@gmail.com>
2024-03-30 21:37:48 +0200

    DAL for Department

---
+++ CrudOperations.DAL/Data/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using CrudOperations.DAL.Data.Configurations;
+using CrudOperations.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrudOperations.DAL.Data
+{
+    internal class ApplicationDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Server = .; Database = MvcCrudOperations; Trusted_Connection = True; MultipleActiveResultSets = True");
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration<Department>(new DepartmentConfigurations());
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+        }
+        
+        public DbSet<Department> Departments {  get; set; }
+
+    }
+}
---
+++ CrudOperations.DAL/Models/Department.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrudOperations.DAL.Models
+{
+    internal class Department
+    {
+        public int Id { get; set; }
+
+        //[Required(ErrorMessage ="Sorry, Code is required")] // it is not Correct to make model work with view Model (PL) Direct
+        public string Code { get; set; }
+
+        //[Required] // Default Name is Required, if you not change it
+        public string Name { get; set; }
+        public DateTime DateOfCreation { get; set; }
+
+    }
+}
